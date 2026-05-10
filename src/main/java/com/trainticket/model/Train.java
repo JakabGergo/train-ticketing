40b81fc3bb4cd195d7354d87eccf2d3510c87735
Jakab Gergo <jakabgergo23@gmail.com>
2026-05-10 12:08:13 +0300

    train model added

---
+++ src/main/java/com/trainticket/model/Train.java
@@ -0,0 +1,34 @@
+package com.trainticket.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalTime;
+
+@Entity
+@Data
+@Component
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "trains")
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
+public class Train extends BaseEntity {
+
+    @Column(nullable = false, unique = true)
+    private String trainNumber;
+
+    @Column(nullable = false)
+    private LocalTime departureTime;
+
+    @Column(nullable = false)
+    private int totalSeats;
+
+    @Column(nullable = false)
+    private int delayMinutes = 0;
+
+    @ManyToOne
+    @JoinColumn(name = "route_id", nullable = false)
+    private Route route;
+}
